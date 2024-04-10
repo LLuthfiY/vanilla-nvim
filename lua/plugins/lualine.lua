17a8e626ff4dba4e1e02643ff71eb54b6ce05706
LLuthfiY <zerodio111@gmail.com>
2024-04-10 20:50:01 +0700

    codeium lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,15 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "catppuccin-mocha",
+        sections = {
+          lualine_a = {
+            file = 1,
+          },
+        },
+      },
+    })
+  end,
 }
