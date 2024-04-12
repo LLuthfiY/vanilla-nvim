7f493d4bf9edc9a297d31b22146be14ef1b81e8e
LLuthfiY <zerodio111@gmail.com>
2024-04-12 19:26:40 +0700

    dashboard

--- lua/configs/alpha.lua
+++ lua/configs/alpha.lua
@@ -2,84 +2,115 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 require("alpha.term")
 
-dashboard.section.terminal.command = "dig reddit.com"
-dashboard.section.terminal.height = 10
-dashboard.section.terminal.width = 50
+local add_spaces = function(text, width)
+  width = width or 32
+  return text .. string.rep(" ", math.max(0, width - vim.fn.strdisplaywidth(text)))
+end
 
 local header = {
-  type = "terminal",
-  command = "viu ~/.config/nvim/static/kalsit_catppuccin.png",
-  height = 25,
-  width = 202,
-  opts = {
-    redraw = true,
-    position = "center",
-  },
+	type = "terminal",
+	command = "viu ~/.config/nvim/static/kalsit_catppuccin.png",
+	height = 22,
+	width = 180,
+	opts = {
+		redraw = true,
+		position = "center",
+	},
 }
 
 local find_files = {
-  type = "button",
-  val = "  Find files",
-  on_press = function()
-    require("telescope.builtin").find_files()
-  end,
-  opts = {
-    position = "center",
-    shortcut = "f          ",
-    keymap = { { "n", "t" }, "f", "<cmd>Telescope find_files<cr>" },
-  },
+	type = "button",
+	val = add_spaces("  Find files"),
+	on_press = function()
+		require("telescope.builtin").find_files()
+	end,
+	opts = {
+    align_shortcut = "right",
+		position = "center",
+		shortcut = "f",
+		keymap = { { "n", "t" }, "f", "<cmd>Telescope find_files<cr>" },
+	},
 }
 
 local new_file = {
+	type = "button",
+	val = add_spaces("  New file"),
+	on_press = function()
+		vim.cmd("enew")
+	end,
+	opts = {
+		position = "center",
+    align_shortcut = "right",
+		shortcut = "n",
+		keymap = { { "n", "t" }, "n", "<cmd>enew<cr>" },
+	},
+}
+
+local recent_files = {
   type = "button",
-  val = "  New file  ",
+  val = add_spaces("  Recently opened files"),
   on_press = function()
-    vim.cmd("enew")
+    require("telescope.builtin").oldfiles()
   end,
   opts = {
     position = "center",
-    shortcut = "n          ",
-    keymap = { { "n", "t" }, "n", "<cmd>enew<cr>" },
+    align_shortcut = "right",
+    shortcut = "r",
+    keymap = { { "n", "t" }, "r", "<cmd>Telescope oldfiles<cr>" },
   },
 }
 
 local config_nvim = {
-  type = "button",
-  val = "  Config    ",
-  on_press = function()
-    require("telescope.builtin").find_files({ cwd = "~/.config/nvim/" })
-  end,
-  opts = {
-    position = "center",
-    shortcut = "c          ",
-    keymap = { { "n", "t" }, "c", "<cmd>Telescope find_files cwd=~/.config/nvim/<cr>" },
-  }
+	type = "button",
+	val = add_spaces("  Config"),
+	on_press = function()
+		require("telescope.builtin").find_files({ cwd = "~/.config/nvim/" })
+	end,
+	opts = {
+		position = "center",
+    align_shortcut = "right",
+		shortcut = "c",
+		keymap = { { "n", "t" }, "c", "<cmd>Telescope find_files cwd=~/.config/nvim/<cr>" },
+	},
 }
 
 local quit = {
-  type = "button",
-  val = "Q  Quit      ",
-  on_press = function()
-    vim.cmd("quit")
-  end,
-  opts = {
-    position = "center",
-    shortcut = "q          ",
-    keymap = { { "n", "t" }, "q", "<cmd>quit<cr>" },
-  },
+	type = "button",
+	val = add_spaces("  Quit"),
+	on_press = function()
+		vim.cmd("quit")
+	end,
+	opts = {
+		position = "center",
+    align_shortcut = "right",
+		shortcut = "q",
+		keymap = { { "n", "t" }, "q", "<cmd>quit<cr>" },
+	},
 }
 
+local function footer()
+	local datetime = os.date(" %Y-%m-%d   %H:%M")
+	local version = vim.version()
+	local nvim_version_info = " v" .. version.major .. "." .. version.minor .. "." .. version.patch
+
+	return datetime .. " || " .. nvim_version_info
+end
+
+dashboard.section.footer.val = footer()
 
 -- Send config to alpha
 --
 dashboard.config.layout = {
-  { type = "padding", val = 2 },
-  header,
-  { type = "padding", val = 6 },
-  find_files,
-  new_file,
-  config_nvim,
-  quit,
+	{ type = "padding", val = 2 },
+	header,
+	{ type = "padding", val = 6 },
+	find_files,
+	new_file,
+  recent_files,
+	config_nvim,
+	quit,
+	{ type = "padding", val = 2 },
+	dashboard.section.footer,
 }
 alpha.setup(dashboard.opts)
 
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-lualine/lualine.nvim",
-  event = {"bufReadPre", "BufNewFile"},
+  -- event = {"bufReadPre", "BufNewFile"},
+  lazy = false,
   config = function()
     require("configs.lualine")
   end,
