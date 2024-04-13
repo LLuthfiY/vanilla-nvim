f70be6d979267a66061f15e575dddd9f393c2d78
LLuthfiY <zerodio111@gmail.com>
2024-04-13 08:08:08 +0700

    options

--- lua/options.lua
+++ lua/options.lua
@@ -2,17 +2,25 @@ vim.g.mapleader = " "
 vim.g.background = "light"
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.shell = "zsh"
+vim.opt.whichwrap:append "<>[]hl"
 
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- vim.cmd("set expandtab")
+-- vim.cmd("set tabstop=2")
+-- vim.cmd("set softtabstop=2")
+-- vim.cmd("set shiftwidth=2")
 
 vim.opt.swapfile = false
+vim.opt.shortmess:append "sI"
 
 vim.o.undofile = true
+vim.o.smartindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
@@ -21,4 +29,4 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.wo.number = true
+vim.o.number = true
