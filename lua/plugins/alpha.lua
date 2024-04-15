f886ff59c43292f5df9531373a211a6d4dc26507
LLuthfiY <zerodio111@gmail.com>
2024-04-15 17:41:43 +0700

    new dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,7 @@
 return {
 	"goolord/alpha-nvim",
   lazy = false,
+  enabled = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
     require("configs.alpha")
---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,56 @@
+return {
+	"nvimdev/dashboard-nvim",
+	event = "VimEnter",
+	config = function()
+		require("dashboard").setup({
+      theme = "doom",
+			preview = {
+				command = "viu -b ",
+				file_path = "~/.config/nvim/static/kalsit_catppuccin.png",
+				file_height = 18,
+				file_width = 150,
+			},
+      config = {
+        center = {
+          {
+            desc = "Find Files",
+            action = "Telescope find_files",
+            key = "f",
+            icon = "  ",
+          },
+          {
+            desc = "New File",
+            action = "enew",
+            key = "n",
+            icon = "  ",
+          },
+          {
+            desc = "Find Text",
+            action = "Telescope live_grep",
+            key = "s",
+            icon = "  ",
+          },
+          {
+            desc = "Update Plugins",
+            action = "Lazy update",
+            key = "u",
+            icon = "  ",
+          },
+          {
+            desc = "Config",
+            action = "Telescope find_files cwd=~/.config/nvim/",
+            key = "c",
+            icon = "  ",
+          },
+          {
+            desc = "Quit",
+            action = "quit",
+            key = "q",
+            icon = "  ",
+          }
+        }
+      }
+      		})
+	end,
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+}
