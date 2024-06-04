759f040cc38d78a885cc09381d81b0f5a167792f
LLuthfiY <zerodio111@gmail.com>
2024-06-05 02:43:21 +0700

    better transparent

--- lua/auto-commands/ui.lua
+++ lua/auto-commands/ui.lua
@@ -2,7 +2,7 @@ local ternary = require("util.tools.ternary")
 local settings = require("configs.Settings")
 local colorscheme = require("util.ui.colorscheme")
 
+vim.cmd("Transparent" .. ternary(settings.UI.transparent, "Enable", "Disable"))
+
 colorscheme.setColorScheme(settings.UI.theme)
 colorscheme.SetHighlight(settings.theme, settings.UI.cmpStyle.kind.coloredBackground)
-
-vim.cmd("Transparent" .. ternary(settings.UI.transparent, "Enable", "Disable"))
--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -6,7 +6,7 @@ return {
 		transparent.clear_prefix("BufferLine")
 		transparent.clear_prefix("NeoTree")
 		transparent.clear_prefix("barbecue")
-		transparent.clear_prefix("lualine")
+		-- transparent.clear_prefix("lualine")
 		transparent.setup()
 	end,
 }
